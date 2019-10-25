00c1dde5975a0af4358ae6656dbd489ec8da780e
Timothy Kelly Jr <tkelly251@yahoo.com>
2019-10-25 17:40:07 -0400

    Update Reading_From_SDCard.m

--- MATLAB/Reading_From_SDCard.m
+++ MATLAB/Reading_From_SDCard.m
@@ -8,16 +8,16 @@ for i=1:length(data)
 end
 
 varlen = size(data.xAccel);
-xVelocity = zeros(varlen(1),1);
+headingVelocity = zeros(varlen(1),1);
 
 BNO055_SAMPLERATE_DELAY_MS = 136; % how often to read data from the board
 ACCEL_VEL_TRANSITION = BNO055_SAMPLERATE_DELAY_MS/1000; % gives us value in per second
 % velocity = accel*dt (dt in seconds)
 
 for i=1:length(data)
-   xVelocity(:,i)=ACCEL_VEL_TRANSITION.*data(i).xAccel./cos((data(i).xOrient).*(pi/180));
+   headingVelocity(:,i)=ACCEL_VEL_TRANSITION.*data(i).xAccel./cos((data(i).xOrient).*(pi/180));
 end
-plot(data.Time, xVelocity);
+plot(data.Time, headingVelocity);
 % subplot(3,1,1)
 % plot(data.Time, data.xAccel)
 % ylabel('m/s^2')
