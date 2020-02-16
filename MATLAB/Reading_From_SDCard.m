cc97a07807f7c867c5354a09044ba10ef56e5029
alexkush2 <rivershack@live.com>
2020-02-16 14:15:09 -0500

    added new plot that has inital value at end

--- MATLAB/Reading_From_SDCard.m
+++ MATLAB/Reading_From_SDCard.m
@@ -16,6 +16,7 @@ t3=1:length(headingVelocity3);
 y1=headingVelocity1;
 y2=headingVelocity2;
 y3=headingVelocity3;
+%%
 
 figure(1)
 subplot(2,1,1)
@@ -48,6 +49,46 @@ for i=1:30:length(headingVelocity3)
     
 end
 
+%% alex's mods
+
+figure(1)
+subplot(2,1,1)
+h1=animatedline('Color','b','Linewidth',1);
+hold on 
+plot(t1,y1)
+h2=animatedline('Color','g','Linewidth',1);
+plot(t2,y2)
+hold on
+h3=animatedline('Color','r','Linewidth',1);
+hold on
+grid on
+plot(t3,y3)
+axis([0,1,-4,10])
+x = linspace(0,length(data3V),length(headingVelocity1));
+ylabel('velocity (m/s)')
+xlabel('Time (minutes)')
+% legend('T1', 'T2', 'T3')
+
+subplot(2,1,2);
+compass(0,0)
+
+
+for i=1:30:length(headingVelocity3)
+    
+    addpoints(h1,x(i),y1(i));%data1.Time*1e-3, headingVelocity1, 'Linewidth', 2);
+    addpoints(h2,x(i),y2(i));
+    addpoints(h3,x(i),y3(i));
+    drawnow
+    [a1,b1]=pol2cart((pi/180)*Orient1(i),1);
+    [a2,b2]=pol2cart((pi/180)*Orient2(i),1);
+    [a3,b3]=pol2cart((pi/180)*Orient3(i),1);
+    subplot(2,1,2)
+    compass(a1,b1)
+    
+end
+hold on;
+[a1,b1]=pol2cart((pi/180)*Orient1(1),1);
+compass(a1,b1,'r')
 %%
 %     plot(data2.Time*1e-3, headingVelocity2, 'Linewidth', 2);
 %     plot(data3.Time*1e-3, headingVelocity3, 'Linewidth', 2);
